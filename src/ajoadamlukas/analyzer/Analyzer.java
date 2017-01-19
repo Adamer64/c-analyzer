8668cb24a19c42b59d26f702be578491e0b00f6c
Adamer64 <mesarc.adam@gmail.com>
2017-01-19 22:49:32 +0100

    mozno aj funguje?

--- src/ajoadamlukas/analyzer/Analyzer.java
+++ src/ajoadamlukas/analyzer/Analyzer.java
@@ -1,5 +1,7 @@
 package ajoadamlukas.analyzer;
 
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.io.IOException;
@@ -19,8 +21,11 @@ public class Analyzer {
     protected List<String> includes = new ArrayList<>();//test
     protected List<String> variables = new ArrayList<>();
     protected List<String> variableNames = new ArrayList<>();
+    protected List<String> newLines = new ArrayList<>();
     protected String patternIncludes = "(#include)(.)+";
     protected String patternVariables = "(\\t|\\s)*(int|char)(\\**)(\\s)(\\**)(\\w+)(\\s*)(=)(\\s*)((\\d+)|(\".*\"))(;)";
+    protected String patternVariablesArray = "(\\t|\\s)*(double|char)(\\**)(\\s)(\\**)(\\w+)(\\[.+\\])(\\s*)(=)(\\s*)(\\{.*\\})(;)";
+    protected boolean firstRun = true;
 
     public void Analyzer() {
     }
@@ -31,9 +36,12 @@ public class Analyzer {
         List<String> newIncludes = new ArrayList<>(); // we can't just add to the original List
         List<String> newVariables = new ArrayList<>();
         List<String> newVariableNames = new ArrayList<>();
+        List<String> newLinesAdded = new ArrayList<>();
         lines = Files.readAllLines(Paths.get(inputFile)); // read every line to the lines List
         Iterator<String> iterator = lines.iterator(); // create iterator
 
+        if(firstRun) {
+            firstRun = false;
             while (iterator.hasNext()) { // is there a line to read
                 next = iterator.next(); // as a variable because we would jump to the next line when using iterator.next() again
                 try {
@@ -43,24 +51,84 @@ public class Analyzer {
                         newVariables.add(next); // add the whole line just to be safe
                         Pattern p = Pattern.compile(patternVariables); // initialize pattern
                         Matcher m = p.matcher(next); // and matcher
-                        if(m.find()) { // did we find anything?
+                        if (m.find()) { // did we find anything?
                             newVariableNames.add(m.group(6)); // if so, add group 5 (the variable name) to the list of variable names
                         }
 
+                    } else if (next.matches(patternVariablesArray)) {
+                        newVariables.add(next);
+                        Pattern p = Pattern.compile(patternVariablesArray); // initialize pattern
+                        Matcher m = p.matcher(next); // and matcher
+                        if (m.find()) { // did we find anything?
+                            newVariableNames.add(m.group(6)); // if so, add group 5 (the variable name) to the list of variable names
+                        }
                     }
-                }
-
-            catch (Exception e) {
+                } catch (Exception e) {
 
                 }
+            }
+
+            includes.addAll(newIncludes); // get all the new includes from the temporary List to the one we will use later
+            variables.addAll(newVariables);
+            variableNames.addAll(newVariableNames);
         }
+        else
+        {
+            System.out.println("Second run:");
+            lines = Files.readAllLines(Paths.get(inputFile)); // read every line to the lines List
+            Iterator<String> varIterator = getVariableNames().iterator();
+            List<String> tempLines = new ArrayList<>();
+            int i=0;
+            String[] nextVar = new String[getVariableNames().size()];
+            while(varIterator.hasNext()) {
+                nextVar[i] = varIterator.next();
+                System.out.println(nextVar[i]);
+                i++;
+            }
+            int k=0;
 
-        includes.addAll(newIncludes); // get all the new includes from the temporary List to the one we will use later
-        variables.addAll(newVariables);
-        variableNames.addAll(newVariableNames);
+                while(k<i) {
+//                    String randomString = Long.toHexString(Double.doubleToLongBits(Math.random()));
+                    String randomString = "a"+Integer.toString(k);
+                    String str = "(.*)(\\s)(\\[*)(" + nextVar[k] + ")(\\]*)(\\s*)(.*)";
+//                    String str = "(.*)"+nextVar[k]+"(.*)";
+//                    String str2 = "("+nextVar[k]+")([^()\\]=\\s\\+\\-\\*]*[^\\(\\[])";
+                    String str2 = "("+nextVar[k]+")([^; \\w])"; //(?<!\w)
+
+                    while (iterator.hasNext()) { // is there a line to read
+                        next = iterator.next(); // as a variable because we would jump to the next line when using iterator.next() again
 
 
-    }
+                        if (next.matches(str)) { // match with defined pattern for include statements
+                            Pattern p = Pattern.compile(str2); // initialize pattern
+                            Matcher m = p.matcher(next); // and matcher
+                            StringBuffer sBuffer = new StringBuffer();
+                            boolean found = false;
+                            while (found = m.find()) { // did we find anything?
+                                String rest="";
+                                System.out.println("WTF: "+ m.group(1) );
+                                rest = m.group(2);
+                                m.appendReplacement(sBuffer, Matcher.quoteReplacement(randomString+rest));
+                            }
+                            System.out.println("Replacing: ");
+                            m.appendTail(sBuffer);
+                            System.out.println(sBuffer.toString());
+                            newLinesAdded.add(sBuffer.toString());
+                        }
+                        else {
+                            newLinesAdded.add(next);
+                        }
+                    }
+                    k++;
+                    tempLines.clear();
+                    tempLines.addAll(newLinesAdded);
+                    newLinesAdded.clear();
+                    iterator = tempLines.listIterator();
+                }
+                newLines.addAll(tempLines);
+
+            }
+        }
 
     public void viewIncludes() {
         System.out.println("Includes:");
@@ -79,6 +147,8 @@ public class Analyzer {
 
     public List<String> getVariables() { return variables; }
 
+    public List<String> getNewLines() { return newLines;}
+
     public void viewVariableNames() {
         System.out.println("Variable names:");
         Iterator<String> iterator = variableNames.iterator(); // iterate
@@ -91,6 +161,16 @@ public class Analyzer {
     public void viewVariables() {
         System.out.println("Variable lines:");
         Iterator<String> iterator = variables.iterator(); // iterate
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next()); // print it
+        }
+        System.out.println();
+//        System.out.println(Long.toHexString(Double.doubleToLongBits(Math.random())));
+    }
+
+    public void viewNewLines() {
+        System.out.println("Output:");
+        Iterator<String> iterator = getNewLines().iterator(); // iterate
         while (iterator.hasNext()) {
             System.out.println(iterator.next()); // print it
         }
